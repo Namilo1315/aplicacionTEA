0daec65271dfa2208237f5d0ff7fe62d63e1a960
nalopez <natalia89751315@gmail.com>
2024-10-12 19:17:50 -0300

    app

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,38 @@
+// service-worker.js
 
-  self.addEventListener("install", function(event) {
-    console.log("Service Worker instalado.");
-  });
-  
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(fetch(event.request));
-  });
-    
+const CACHE_NAME = 'autismo-naty-cache-v1'; // Cambia esto si actualizas los archivos en el futuro
+const urlsToCache = [
+  './', // Incluye el index.html
+  './index.html',
+  './pictogramas.html',
+  './animales.html',
+  './frutas.html',
+  './juegos.html',
+  './emociones.html',
+  './numeros.html',
+  './egulacion.html',
+  './preposiciones.html',
+  './imagenes/icon1.png', // Incluye tus iconos
+  './imagenes/icon2.png',
+ 
+];
+
+// Instala el service worker y cachea los archivos necesarios
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Archivos cacheados:', urlsToCache);
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Responde con los archivos cacheados
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Si hay una respuesta en caché, devuélvela; de lo contrario, busca en la red
+      return response || fetch(event.request);
+    })
+  );
+});
