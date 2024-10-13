be414dbeaf73c7acec30a0e9deb5be1f1421451c
nalopez <natalia89751315@gmail.com>
2024-10-13 13:39:19 -0300

    aplicacion

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
-// service-worker.js
-
-const CACHE_NAME = 'autismo-naty-cache-v1'; // Cambia esto si actualizas los archivos en el futuro
+const CACHE_NAME = 'autismo-naty-cache-v1';
 const urlsToCache = [
   './index.html',
   './pictogramas.html',
   './animales.html',
-  './frutas.html',       // Asegúrate de que esta línea esté incluida
-  './juegos.html',       // Y que esta línea también esté incluida
+  './frutas.html',
+  './juegos.html',
   './emociones.html',
   './numeros.html',
-  './regulacion.html',   // Incluye todos los HTML necesarios
+  './regulacion.html',
   './preposiciones.html',
   './imagenes/icon1.png',
   './imagenes/icon2.png',
-  './estilos.css', // Ruta a tu archivo CSS
-  './index.js', 
-   './juegos.js', 
-]
+  './estilos.css', // Asegúrate de que todos los archivos CSS necesarios están cacheados
+  './index.js',    
+  './juegos.js', 
+];
 
-// Instala el service worker y cachea los archivos necesarios
+
+
+// Instala el Service Worker y cachea los archivos necesarios
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -28,12 +28,35 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Responde con los archivos cacheados
+// Responde con los archivos cacheados si están disponibles, o busca en la red
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       // Si hay una respuesta en caché, devuélvela; de lo contrario, busca en la red
-      return response || fetch(event.request);
+      return response || fetch(event.request).then((networkResponse) => {
+        // Abrir caché y almacenar la respuesta en caché dinámica si no está cacheada
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
     })
   );
 });
+
+// Evento de activación del Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antigua:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
